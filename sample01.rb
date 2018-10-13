258cfa55e408242bbbf9219ab45d9f9e3b3fe8bd
ユウ <sukesuke0718@gmail.com>
2018-10-13 14:12:48 +0900

    新規作成

---
+++ sample01.rb
@@ -0,0 +1,16 @@
+# コメント
+# alt + R　で実行できる
+
+for i in 1 .. 10 do
+
+  if i % 3 != 0 then
+    puts i
+  end
+
+end
+
+# 辞書
+array = {'国語' => 70, '数学' => 90, '英語' => 78,'理科' => 66}
+array01 = ['a', 'b', 'c', 'd']
+puts "国語は#{array["国語"]}点です"
+puts array01
